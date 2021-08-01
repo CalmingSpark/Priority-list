fbd3f8a92a12e7ae9f13e6d53b3e793f6ac245df
CalmingSpark <79797054+CalmingSpark@users.noreply.github.com>
2021-08-01 23:59:33 +0300

    Added gray theme

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 const fs = require('fs');
 var isAdd = false;
 var isEdit = false;
+var styleColors = 'default';
+
+var btn_checkHTML = '<img src="resources/default/Check_mark.png" />';
+var btn_editHTML = '<img src="resources/default/Edit.png" />';
+var btn_cancelHTML = '<img src="resources/default/Close.png" />';
+var btn_checkAddHTML = '<img src="resources/default/Check_mark_add.png" />';
+var btn_waitingHTML = '<img src="resources/default/Close_add.png" />';
 
 window.addEventListener('load', function() {
 	fs.readFile("C:/Priority-list/input_File.json", "utf8", function(error,data){ 
 	if(error) throw error; 
 	parsingFile(data);
 	});
+	SetColors('default');
 })
 
 function parsingFile(data) {
 	var tbody = document.getElementById('tMainBody');
 	
     let c = data.split('},\n').length;
-
     for (var i = 1; i <= c ; i++) {
     	let start = '{\n"id":"' + i;
     	if (i != c) {
@@ -101,7 +108,6 @@ function AddRow(){
 		    a.parentElement.removeChild(a);
 		    isAdd = false;
 		  });
-	    btn_check.innerHTML = '<img src="resources/default/Check_mark_add.png" />';
 	    td3.appendChild(btn_check);
 
 	    btn_waiting = document.createElement('button');
@@ -111,7 +117,6 @@ function AddRow(){
 		    a.parentElement.removeChild(a);
 		    isAdd = false;
 		  });
-	    btn_waiting.innerHTML = '<img src="resources/default/Close_add.png" />';
 	    td3.appendChild(btn_waiting);
 
 	    row.appendChild(td1);
@@ -119,6 +124,9 @@ function AddRow(){
 	    row.appendChild(td3);
 	    tbody.appendChild(row);
 	    isAdd = true;
+	    SetColors(styleColors);
+	    btn_check.innerHTML = btn_checkAddHTML;
+	    btn_waiting.innerHTML = btn_waitingHTML;
 	}
 }
 function AddMaintbl(input_progect, input_description, index) {
@@ -140,10 +148,9 @@ function AddMaintbl(input_progect, input_description, index) {
     	var td3 = document.createElement("td");
     }
     
-
     btn_check = document.createElement('button');
     btn_check.id = 'check';
-    btn_check.innerHTML = '<img src="resources/default/Check_mark.png" />';
+    btn_check.innerHTML = btn_checkHTML;
     btn_check.addEventListener('click', function() {
 	    	result = confirm("Закрыть проект?");
 			if (result) {var a = this.closest('tr'); a.parentElement.removeChild(a); Save();}
@@ -152,11 +159,14 @@ function AddMaintbl(input_progect, input_description, index) {
 
     btn_edit = document.createElement('button');
     btn_edit.id = 'edit';
+    btn_edit.innerHTML = btn_editHTML;
     btn_edit.addEventListener('click', function() {
 	    Edit(this.closest('tr'),$(this).closest('tr').index());
 	});
-    btn_edit.innerHTML = '<img src="resources/default/Edit.png" />';
     td3.appendChild(btn_edit);
+
+    if (styleColors == 'default') {td3.style.setProperty('--maint-color_td_last', "#8d6646");}
+	if (styleColors == 'gray') {td3.style.setProperty('--maint-color_td_last', "#efefef");}
 
     if (index < 0) {
 	    row.appendChild(td1);
@@ -165,7 +175,6 @@ function AddMaintbl(input_progect, input_description, index) {
 	    tbody.appendChild(row);
 	}
     Save();
-    SetColors('default');
 }
 
 function Save() {
@@ -222,6 +231,8 @@ function Edit(row,index){
 			var input = document.createElement('TEXTAREA');
 			row_text = row.childNodes[i].innerHTML;
 			input.innerHTML = row_text;
+			if (styleColors == 'default'){input.style.setProperty('--textarea-colorText', "#ddd");}
+			if (styleColors == 'gray'){input.style.setProperty('--textarea-colorText', "#040404");}
 			if(i == 0) {var input_progect_new = input;}
 			if(i == 1) {var input_description_new = input;}
 			newCell.appendChild(input);
@@ -230,6 +241,7 @@ function Edit(row,index){
 		var buttonCell = newRow.insertCell(2);
 		btn_check = document.createElement('button');
 	    btn_check.id = 'add';
+	    btn_check.innerHTML = btn_checkHTML;
 	    btn_check.addEventListener('click', function() {
 		    AddMaintbl(input_progect_new.value, input_description_new.value, index);
 		    var a = this.closest('tr'); 
@@ -237,11 +249,11 @@ function Edit(row,index){
 		    Save();
 		    isEdit = false;
 		  });
-	    btn_check.innerHTML = '<img src="resources/default/Check_mark.png" />';
 	    buttonCell.appendChild(btn_check);
 
 	    btn_cancel = document.createElement('button');
 	    btn_cancel.id = 'cancel';
+	    btn_cancel.innerHTML = btn_cancelHTML;
 	    btn_cancel.addEventListener('click', function() {
 	    	AddMaintbl(input_progect.innerHTML, input_description.innerHTML, index);
 		    var a = this.closest('tr');
@@ -249,8 +261,9 @@ function Edit(row,index){
 		    Save();
 		    isEdit = false;
 		  });
-	    btn_cancel.innerHTML = '<img src="resources/default/Close.png" />';
 	    buttonCell.appendChild(btn_cancel);
+	    if (styleColors == 'default') {buttonCell.style.setProperty('--maint-color_td_last', "#8d6646");}
+	    if (styleColors == 'gray') {buttonCell.style.setProperty('--maint-color_td_last', "#efefef");}
 
 	    row.parentElement.removeChild(row);
 	    isEdit = true;
@@ -262,6 +275,8 @@ function SetColors(styleColors){
 	var tthead = document.getElementById('tMainThead');
 	var tbody = document.getElementById('tMainBody');
 	var tbodyLast = document.querySelector('tbody tr:last-child td:last-child');
+	var tbodyAdd = document.getElementById('addtbody');
+	var tbodyAddLast = document.querySelector('.added-table tbody tr:last-child td:last-child');
 
 	if(styleColors == 'default'){
 		body.style.setProperty('--body-color', "#ac7e59");
@@ -270,6 +285,24 @@ function SetColors(styleColors){
 		tthead.style.setProperty('--maint-color_thead', "#473424");
 		tthead.style.setProperty('--maint-colorText_thead', "#ddd");
 		tbodyLast.style.setProperty('--maint-color_td_last', "#8d6646");
+		var rowLength = tbody.rows.length;
+		for (i = 0; i < rowLength; i++){
+			var oCells = tbody.rows.item(i).cells;
+			var cellVal = oCells.item(2);
+			cellVal.style.setProperty('--maint-color_td_last', "#8d6646");
+			cellVal.childNodes[0].innerHTML = '<img src="resources/default/Check_mark.png" />';
+			cellVal.childNodes[1].innerHTML = '<img src="resources/default/Edit.png" />';
+		}
+
+		document.getElementById('closeWindowImg').src="resources/default/CloseWindow.png";
+		document.getElementById('addRowImg').src="resources/default/Add.png";
+		document.getElementById('waitingImg').src="resources/default/Waiting.png";
+		document.getElementById('settingsImg').src="resources/default/Settings.png";
+		document.getElementById('saveImg').src="resources/default/Save.png";
+
+		tbodyAdd.style.setProperty('--addt-color_td', "#387051");
+		tbodyAdd.style.setProperty('--addt-colorText_td', "#ddd");
+		tbodyAddLast.style.setProperty('--addt-color_td_last' , "#468d66");
 	}
 
 	if(styleColors == 'gray'){
@@ -283,11 +316,42 @@ function SetColors(styleColors){
 			var oCells = tbody.rows.item(i).cells;
 			var cellVal = oCells.item(2);
 			cellVal.style.setProperty('--maint-color_td_last', "#efefef");
+			cellVal.childNodes[0].innerHTML = '<img src="resources/gray/Check_mark.png" />';
+			cellVal.childNodes[1].innerHTML = '<img src="resources/gray/Edit.png" />';
 		}
+
+		document.getElementById('closeWindowImg').src="resources/gray/CloseWindow.png";
+		document.getElementById('addRowImg').src="resources/gray/Add.png";
+		document.getElementById('waitingImg').src="resources/gray/Waiting.png";
+		document.getElementById('settingsImg').src="resources/gray/Settings.png";
+		document.getElementById('saveImg').src="resources/gray/Save.png";
+
+		tbodyAdd.style.setProperty('--addt-color_td', "#8bc5a5");
+		tbodyAdd.style.setProperty('--addt-colorText_td', "#040404");
+		tbodyAddLast.style.setProperty('--addt-color_td_last' , "#a3d1b7");
 	}
 }
 
 function Settings(){
 	/*var myWindow=window.open("/settings.html","DescriptiveWindowName","resizable,scrollbars,status");*/
-	SetColors('gray');
+	if(styleColors == 'default') {
+		styleColors = 'gray';
+		btn_checkHTML = '<img src="resources/gray/Check_mark.png" />';
+		btn_editHTML = '<img src="resources/gray/Edit.png" />';
+		btn_cancelHTML = '<img src="resources/gray/Close.png" />';
+		btn_checkAddHTML = '<img src="resources/gray/Check_mark_add.png" />';
+		btn_waitingHTML = '<img src="resources/gray/Close_add.png" />';
+		SetColors(styleColors);
+		return;
+	}
+	if(styleColors == 'gray') {
+		styleColors = 'default';
+		btn_checkHTML = '<img src="resources/default/Check_mark.png" />';
+		btn_editHTML = '<img src="resources/default/Edit.png" />';
+		btn_cancelHTML = '<img src="resources/default/Close.png" />';
+		btn_checkAddHTML = '<img src="resources/default/Check_mark_add.png" />';
+		btn_waitingHTML = '<img src="resources/default/Close_add.png" />';
+		SetColors(styleColors);
+		return;
+	}
 }
